0e5ce26b93ac35dc28a5335b85af78ec6e67c719
Gustavo Borges Hertz <127430409+gustavohertz@users.noreply.github.com>
2023-07-31 08:21:34 -0300

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int resultado = Somar(10, 80);
+        Console.WriteLine(resultado);
+    }
+    static int Somar(int a, int b)
+    {
+        return a + b;
+    }
+}
